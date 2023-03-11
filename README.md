0dfa69457b83585d8a4eb5077f4da1e20b21b871
Tom Jenkinson <tom@tjenkinson.me>
2023-03-11 16:12:28 +0000

    wip

---
+++ README.md
@@ -0,0 +1,174 @@
+# Dynamic Marquee React
+
+TODO
+
+A small library for creating marquees.
+
+Features:
+
+- You can change the rate on the fly.
+- Direction can either be up/down or right/left.
+- Width/height of items and container is allowed to change.
+- Container width/height is updated correctly to match maximum size of current items.
+- You can add an item at any time when space is available, and it will start off screen.
+
+A [`loop()`](#loop) helper function is also provided which makes creating a carousel with looping content simple.
+
+# Demo
+
+[![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/tjenkinson/dynamic-marquee/tree/master/demo?title=Dynamic%20Marquee%20Demo&file=src%2Fmain.ts)
+
+View the code in "[demo](./demo)".
+
+# Installation
+
+```
+npm install --save dynamic-marquee
+```
+
+```js
+import { Marquee } from 'dynamic-marquee';
+```
+
+or
+
+```html
+<script
+  type="text/javascript"
+  src="https://cdn.jsdelivr.net/npm/dynamic-marquee@2"
+></script>
+
+<script type="text/javascript">
+  const Marquee = dynamicMarquee.Marquee;
+</script>
+```
+
+thanks to [jsDelivr](https://github.com/jsdelivr/jsdelivr).
+
+# Usage
+
+## Construct Marquee Instance
+
+### With Default Options
+
+```js
+const marquee = new Marquee(document.getElementById('marquee'));
+```
+
+### With Custom Options
+
+```js
+const marquee = new Marquee(document.getElementById('marquee'), {
+  rate: 20, // 20 pixels/s downwards
+  upDown: true, // downwards instead of to the right
+  startOnScreen: false, // start on screen
+});
+```
+
+## Append Item
+
+You can add DOM elements, or just a string (which will automatically be wrapped in a div).
+
+**Each DOM element is only allowed on the marquee at one time.**
+
+```js
+const $item = document.createElement('div');
+$item.textContent = 'testing123';
+marquee.appendItem($item);
+```
+
+You are only allowed to append an item when there is room. You can check this like so:
+
+```js
+if (marquee.isWaitingForItem()) {
+  marquee.appendItem($item);
+}
+```
+
+`appendItem` also takes an optional second param `config` object, which can contain:
+
+- `metadata`: The value of this will be provided back to you in `onItemRequired`.
+- `snapToNeighbour`: If `true` the item will snap to the end of the neighbouring item instead of starting off screen.
+
+You can be notified when an item is required with
+
+```js
+marquee.onItemRequired(({ touching }) => {
+  // For convenience if you have an item ready to go you can just return it
+  // in place of `marquee.appendItem($item);`
+
+  // If the new item would be touching another then `touching`
+  // will be set to an object that contains `$el` and `metadata` of
+  // the item it will be touching.
+  // This can be used to determine if a separate should be added.
+  // See loop.js for an example.
+  return $item;
+});
+```
+
+**Do not perform any long running tasks in this method as it will block rendering.**
+
+If you need to perform some work in this method consider wrapping it in a `setTimeout` with delay 0.
+
+## Change the scroll rate? (px/s)
+
+You can change the rate at any time, and set to 0 to pause.
+
+```js
+marquee.setRate(-20);
+```
+
+Note if you change the direction, `isWaitingForItem()` will change to `false`, and `onItemRequired()` will be called again when needed.
+
+## Reset
+
+To remove all items call
+
+```js
+marquee.clear();
+```
+
+You should also call this before removing the marquee from the DOM if you no longer need it to ensure that all timers are cleaned up and garbage collection can occur.
+
+## When has an item been removed?
+
+You can be notified when an item has been removed with:
+
+```js
+marquee.onItemRemoved(($el) => {
+  // $el has just been removed
+});
+```
+
+## When have all items finished scrolling?
+
+You can be notified when the scroller is empty with:
+
+```js
+marquee.onAllItemsRemoved(() => {
+  //
+});
+```
+
+You can check at any time with:
+
+```js
+marquee.getNumItems();
+```
+
+# Loop
+
+A `loop()` function is provided for making looping content simple.
+
+You provide an array of functions which return a DOM element, or string for that item. You can update this on the fly by calling the provided `update()` method.
+
+**When returning DOM elements each function should build the element from scratch, as the same DOM element is not allowed to appear on the marquee multiple times.**
+
+```js
+const $marquee = document.getElementById('marquee');
+const marquee = new Marquee($marquee);
+const control = loop(marquee, [() => 'item 1', () => 'item 2']);
+
+// later
+control.update([() => 'new item 1', () => 'new item 2']);
+```
---
+++ src/marquee.tsx
@@ -0,0 +1,275 @@
+import { Marquee as MarqueeLib } from 'dynamic-marquee';
+import React, {
+  ReactNode,
+  Children,
+  PropsWithChildren,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from 'react';
+import { createPortal } from 'react-dom';
+
+// TODO update doc comments
+
+export type MarqueeOpts = {
+  rate?: number;
+  upDown?: boolean;
+  startOnScreen?: boolean;
+};
+
+function createContainer() {
+  const $el = document.createElement('div');
+  $el.style.all = 'unset';
+  $el.style.display = 'block';
+  return $el;
+}
+
+function IdGenerator() {
+  const ids = new Set();
+  return {
+    generate() {
+      const base = `${performance.now()}`;
+      let id = base;
+      for (let i = 0; ids.has(id); i++) {
+        id = `${base}:${i}`;
+      }
+      ids.add(id);
+      return id;
+    },
+    release(id: string) {
+      ids.delete(id);
+    },
+  };
+}
+
+function WatchSize({
+  marqueeInstance,
+  onChange,
+  children,
+}: PropsWithChildren<{
+  marqueeInstance: MarqueeLib;
+  onChange: (size: number) => void;
+}>) {
+  const [$container] = useState(createContainer());
+  const currentOnChange = useRef(onChange);
+  currentOnChange.current = onChange;
+
+  useLayoutEffect(() => {
+    if (!$container) return;
+
+    const { getSize, onSizeChange, stopWatching } =
+      marqueeInstance.watchItemSize($container);
+
+    const send = () => currentOnChange.current(getSize());
+
+    onSizeChange(() => send());
+
+    send();
+
+    return () => stopWatching();
+  }, [$container, marqueeInstance]);
+
+  return createPortal(children, $container);
+}
+
+export function Marquee({
+  children,
+  ...marqueeOpts
+}: PropsWithChildren<MarqueeOpts>) {
+  const filteredChildren = Children.toArray(children);
+  if (filteredChildren.length === 0) {
+    return <div />;
+  }
+  return (
+    <MarqueeInternal {...marqueeOpts} filteredChildren={filteredChildren} />
+  );
+}
+
+type Placeholder = {
+  $placeholder: HTMLDivElement;
+  key: string;
+  childIndex: number;
+  inDom: boolean;
+  appended: boolean;
+};
+
+// TODO take opts individually and make sure upDown works
+const MarqueeInternal = React.memo(
+  ({
+    filteredChildren,
+    rate,
+    upDown,
+    startOnScreen,
+  }: MarqueeOpts & { filteredChildren: ReactNode[] }) => {
+    const [rateInitial] = useState(rate);
+    const [startOnScreenInitial] = useState(startOnScreen);
+    const [upDownInitial] = useState(upDown);
+    const [idGenerator] = useState(IdGenerator());
+    const [, setRenderTrigger] = useState<object | null>(null);
+    const [$container, setContainer] = useState<HTMLDivElement | null>(null);
+    const placeholders = useRef<Placeholder[]>([]);
+    const nextChildIndex = useRef(0);
+    const [marqueeInstance, setMarqueeInstance] = useState<MarqueeLib | null>(
+      null
+    );
+    const nextItemTouching = useRef(false);
+    const itemSizes = useRef<number[]>([]);
+    const childrenCount = useRef(filteredChildren.length);
+    childrenCount.current = filteredChildren.length;
+
+    useLayoutEffect(() => {
+      if (itemSizes.current.length > filteredChildren.length) {
+        itemSizes.current = itemSizes.current.slice(0, filteredChildren.length);
+      }
+    }, [filteredChildren.length]);
+
+    useEffect(() => {
+      if (!$container) return;
+
+      // (1) create the marquee instance
+      const marquee = new MarqueeLib($container, {
+        upDown: upDownInitial,
+        startOnScreen: startOnScreenInitial,
+        rate: rateInitial,
+      });
+      setMarqueeInstance(marquee);
+
+      return () => marquee.clear();
+    }, [
+      $container,
+      rateInitial,
+      setMarqueeInstance,
+      startOnScreenInitial,
+      upDownInitial,
+    ]);
+
+    useEffect(() => {
+      if (!marqueeInstance) return;
+
+      const createPlaceholders = (sizeToFill: number) => {
+        placeholders.current
+          .filter(({ appended }) => !appended)
+          .forEach(
+            ({ childIndex }) => (sizeToFill -= itemSizes.current[childIndex])
+          );
+
+        let zeroIncreaseCounter = 0;
+        while (
+          sizeToFill > 0 &&
+          // if all sizes end up being 0 prevent an infinite loop
+          zeroIncreaseCounter < itemSizes.current.length
+        ) {
+          // (3) create a placeholder but don't put it in the dom yet
+          const childIndex = nextChildIndex.current;
+          const childSize = itemSizes.current[childIndex];
+          if (childSize === 0) {
+            zeroIncreaseCounter++;
+          } else {
+            zeroIncreaseCounter = 0;
+          }
+          sizeToFill -= childSize;
+
+          const $placeholder = createContainer();
+
+          placeholders.current.push({
+            $placeholder,
+            key: idGenerator.generate(),
+            childIndex,
+            inDom: false,
+            appended: false,
+          });
+
+          nextChildIndex.current =
+            (nextChildIndex.current + 1) % childrenCount.current;
+        }
+
+        setRenderTrigger({});
+      };
+
+      marqueeInstance.onItemRequired(({ touching }) => {
+        // todo can to fill actually be 0?
+        // (6) there is space for a new item, so create the placeholder for it
+        nextItemTouching.current = !!touching;
+        createPlaceholders(marqueeInstance.getGapSize());
+      });
+
+      marqueeInstance.onItemRemoved(($el) => {
+        placeholders.current = placeholders.current.filter(
+          ({ $placeholder, key }) => {
+            if ($el === $placeholder) {
+              idGenerator.release(key);
+              return false;
+            }
+            return true;
+          }
+        );
+      });
+
+      // (2) create the first placeholder for the first item
+      createPlaceholders(marqueeInstance.getGapSize());
+    }, [idGenerator, marqueeInstance]);
+
+    useEffect(() => {
+      if (!marqueeInstance || rate === undefined) return;
+      marqueeInstance.setRate(rate);
+    }, [marqueeInstance, rate]);
+
+    useEffect(() => {
+      if (!marqueeInstance) return;
+
+      while (marqueeInstance.isWaitingForItem()) {
+        const toAppend = placeholders.current.find(
+          ({ inDom, appended }) => inDom && !appended
+        );
+        if (!toAppend) {
+          // TODO comment that we misjudged size or initial render
+          return;
+        }
+
+        // TODO comment on mutation
+        toAppend.appended = true;
+
+        // (5) at this point we should have rendered and the element will be
+        // in the dom in the portal with content in, so append it to the marquee
+        console.log('append placeholder');
+        marqueeInstance.appendItem(toAppend.$placeholder, {
+          // TODO not that onItemRequired may be called here and update this ref
+          snapToNeighbour: nextItemTouching.current,
+        });
+      }
+    });
+
+    return (
+      <React.Fragment>
+        <div
+          ref={setContainer}
+          style={{ all: 'unset', display: 'block', height: '100%' }}
+        />
+        {/* (4) create the portal, putting the new placeholders in the dom (if there are any) */}
+        {placeholders.current.map((placeholder) => {
+          const { $placeholder, key, childIndex } = placeholder;
+          placeholder.inDom = true;
+          const child = filteredChildren[childIndex];
+          return child ? createPortal(child, $placeholder, key) : null;
+        })}
+
+        {marqueeInstance
+          ? filteredChildren.map((child, i) => (
+              <WatchSize
+                key={i}
+                marqueeInstance={marqueeInstance}
+                onChange={(size) => {
+                  itemSizes.current[i] = size;
+                }}
+              >
+                {child}
+              </WatchSize>
+            ))
+          : null}
+      </React.Fragment>
+    );
+  }
+);
+
+MarqueeInternal.displayName = 'MarqueeInternal';
